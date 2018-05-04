f56beb878e3b75bdb06ce66467079c4d431fe740
EC2 Default User <ec2-user@ip-172-31-28-37.us-east-2.compute.internal>
2018-05-04 18:02:42 +0000

    first commit

---
+++ port.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#top_menu").click(function(){
+    $(".top_navi").slideToggle();
+  });
+});
+$(function(){
+    $('a[href^=#]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
+
