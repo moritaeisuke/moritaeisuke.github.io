f17e0b5578d94b999151cb268e64879c10c76d0d
EC2 Default User <ec2-user@ip-172-31-28-37.us-east-2.compute.internal>
2018-05-11 08:48:03 +0000

    sa-do commit

--- port.js
+++ port.js
@@ -13,4 +13,27 @@ $(function(){
         return false;
     });
 });
+$(function(){
+var topBtn=$('#pagetop');
+topBtn.hide();
 
+ 
+
+//◇ボタンの表示設定
+$(window).scroll(function(){
+  if($(this).scrollTop()>800){
+
+    //---- 画面を80pxスクロールしたら、ボタンを表示する
+    topBtn.fadeIn();
+
+  }else{
+
+    //---- 画面が80pxより上なら、ボタンを表示しない
+    topBtn.fadeOut();
+
+  } 
+});
+
+
+
+});
